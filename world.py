c4a6ba7c5b6a129b19dec4ceeaee13071becc119
augustelalande <auguste.lalande@gmail.com>
2016-11-25 18:44:26 -0500

    replace random_in_range with built in function

--- world.py
+++ world.py
@@ -40,18 +40,6 @@ class World(object):
         for start, end in zip(corners, corners[1:]):
             self._data.addLine(start, end, radius=0.2)
 
-    def _random_in_range(self, min_v, max_v):
-        """Generate random value in a given range using a uniform distribution.
-
-        Args:
-            min_v: Minimum value.
-            max_v: Maximum value.
-
-        Returns:
-            Random value.
-        """
-        return (max_v - min_v) * np.random.random_sample() + min_v
-
     def generate_obstacles(self, density=0.05, moving_obstacle_ratio=0.20,
                            seed=None):
         """Generates randomly scattered obstacles to the world.
@@ -72,18 +60,18 @@ class World(object):
         obstacle_area = int(field_area * density)
 
         while obstacle_area > 0:
-            radius = self._random_in_range(0.4, 2.0)
+            radius = np.random.uniform(0.4, 2.0)
             center_x_range = (self._x_min + radius, self._x_max - radius)
             center_y_range = (self._y_min + radius, self._y_max - radius)
-            center_x = self._random_in_range(*center_x_range)
-            center_y = self._random_in_range(*center_y_range)
+            center_x = np.random.uniform(*center_x_range)
+            center_y = np.random.uniform(*center_y_range)
             obstacle_area -= np.pi * radius ** 2
 
             # Only some obstacles should be moving.
             if np.random.random_sample() >= moving_obstacle_ratio:
                 velocity = 0.0
             else:
-                velocity = self._random_in_range(-30.0, 30.0)
+                velocity = np.random.uniform(-30.0, 30.0)
 
             obstacle = Obstacle(velocity, radius)
             obstacle.x = center_x
