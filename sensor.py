b88c693f5c2bfa47d86d4086538c69bae6cb32f9
augustelalande <auguste.lalande@gmail.com>
2016-11-25 13:26:37 -0500

    add sensor

---
+++ sensor.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*=
+
+import math
+import numpy as np
+import director.vtkAll as vtk
+import director.objectmodel as om
+
+
+class Sensor(object):
+
+    def __init__(self, world, num_rays=20, radius=8,
+                 min_angle=-90, max_angle=90):
+
+        self.num_rays = num_rays
+        self.radius = radius
+        self.min_angle = math.radians(min_angle)
+        self.max_angle = math.radians(max_angle)
+
+        self.locator = vtk.vtkCellLocator()
+        self.locator.SetDataSet(world.to_polydata())
+        self.locator.BuildLocator()
+
+        self.results = np.zeros(self.num_rays)
+        self.distances = np.zeros(self.num_rays)
+        self.intersections = [[0, 0, 0] for i in range(self.num_rays)]
+
+        self._update_rays(0)
+
+    def update(self, x, y, theta):
+        self._update_rays(theta)
+        origin = np.array([x, y, 0])
+
+        for i in range(self.num_rays):
+            res, dist, inter = self._cast_ray(origin, origin + self.rays[i])
+            self.results[i] = res
+            self.distances[i] = dist
+            self.intersections[i] = inter
+
+    def _update_rays(self, theta):
+        self.rays = [
+            np.array([
+                math.cos(
+                    theta + self.min_angle + i *
+                    (self.max_angle - self.min_angle) / (self.num_rays - 1)
+                ) * self.radius,
+                math.sin(
+                    theta + self.min_angle + i *
+                    (self.max_angle - self.min_angle) / (self.num_rays - 1)
+                ) * self.radius,
+                0
+            ])
+            for i in range(self.num_rays)
+        ]
+
+    def _cast_ray(self, start, end):
+
+        tolerance = 0.0                 # intersection tolerance
+        pt = [0.0, 0.0, 0.0]            # coordinate of intersection
+        distance = vtk.mutable(0.0)     # distance of intersection
+        pcoords = [0.0, 0.0, 0.0]       # location within intersected cell
+        subID = vtk.mutable(0)          # subID of intersected cell
+
+        result = self.locator.IntersectWithLine(start, end, tolerance,
+                                                distance, pt, pcoords, subID)
+
+        return result, distance, pt
--- simulator.py
+++ simulator.py
@@ -3,6 +3,7 @@
 import numpy as np
 from robot import Robot
 from world import World
+from sensor import Sensor
 from PythonQt import QtGui
 from director import applogic
 from director import vtkAll as vtk
@@ -10,13 +11,14 @@ from director import objectmodel as om
 from director import visualization as vis
 from director.consoleapp import ConsoleApp
 from director.timercallback import TimerCallback
+from director.debugVis import DebugData
 
 
 class Simulator(object):
 
     """Simulator."""
 
-    def __init__(self, robot, world):
+    def __init__(self, robot, world, sensor):
         """Constructs the simulator.
 
         Args:
@@ -25,6 +27,7 @@ class Simulator(object):
         """
         self._robot = robot
         self._world = world
+        self._sensor = sensor
         self._app = ConsoleApp()
         self._view = self._app.createView(useGrid=False)
         self._robot_frame = None
@@ -52,6 +55,22 @@ class Simulator(object):
         t.RotateZ(np.degrees(self._robot.theta))
         self._robot_frame.getChildFrame().copyFrame(t)
 
+    def _update_sensor(self):
+        """Updates sensor's rays."""
+        d = DebugData()
+
+        origin = np.array([self._robot.x, self._robot.y, 0])
+
+        for result, intersection, ray in zip(self._sensor.results,
+                                             self._sensor.intersections,
+                                             self._sensor.rays):
+            if result:
+                d.addLine(origin, intersection, color=[1, 0, 0], radius=0.05)
+            else:
+                d.addLine(origin, origin + ray, color=[0, 0, 1], radius=0.05)
+
+        vis.updatePolyData(d.getPolyData(), 'rays', colorByName='RGB255')
+
     def run(self):
         """Launches and displays the simulator."""
         widget = QtGui.QWidget()
@@ -72,10 +91,13 @@ class Simulator(object):
     def tick(self):
         """Update simulation clock."""
         self._robot.move()
+        self._sensor.update(self._robot.x, self._robot.y, self._robot.theta)
         self._update_robot()
+        self._update_sensor()
 
 if __name__ == "__main__":
     robot = Robot()
     world = World(120, 100).add_obstacles()
-    sim = Simulator(robot, world)
+    sensor = Sensor(world)
+    sim = Simulator(robot, world, sensor)
     sim.run()
